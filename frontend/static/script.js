9c1f35907550617ea983c88896d28e6355bc9ba8
Yiwen Li <yiwenknows@gmail.com>
2024-01-03 12:37:38 -0500

    scatter plot

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -29,6 +29,7 @@ d3.json('/players/').then((fetchedData) => {
   updateDropdown();
   createAgeHistogram(data);
   createFootBarChart(data);
+  createScatterPlot(data);
 });
 
 function createTable(columns) {
@@ -246,4 +247,49 @@ function createFootBarChart(data) {
     .attr('width', x.bandwidth())
     .attr('height', (d) => height - y(d.count))
     .attr('fill', '#69b3a2');
+}
+
+function createScatterPlot(data) {
+  // Define dimensions and margins
+  const margin = { top: 10, right: 30, bottom: 30, left: 60 };
+  const width = 460 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  // Append SVG object to the scatterPlot div
+  const svg = d3
+    .select('#scatterPlot')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  // Add X axis
+  const x = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => +d.Age)])
+    .range([0, width]);
+  svg
+    .append('g')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => +d.Rating)])
+    .range([height, 0]);
+  svg.append('g').call(d3.axisLeft(y));
+
+  // Add dots
+  svg
+    .append('g')
+    .selectAll('dot')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', (d) => x(d.Age))
+    .attr('cy', (d) => y(d.Rating))
+    .attr('r', 5)
+    .style('fill', '#69b3a2');
 }
